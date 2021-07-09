90db02779a4a04564ebedbc83ed6ceaeed7a4605
Matias Galante <CarpalCarp@users.noreply.github.com>
2021-07-09 15:46:52 -0600

    finished projects component

--- src/app/projects/project.model.ts
+++ src/app/projects/project.model.ts
@@ -1,4 +1,5 @@
 export class Project {
-    constructor(public title: string, public subtitle: string, public src: string) {
+    constructor(public title: string, public subtitle: string, public src: string, public url: string, public description: string) {
+
     }
 }
